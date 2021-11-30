ee207b94d7adec127b411abbfad858c8528b149e
suchcka90 <danil-sheptalin@mail.ru>
2021-11-30 17:33:33 +0700

    added few basics user controllers,User entity and some templates for this all

---
+++ src/Controller/Main/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+    public function index()
+    {
+
+    }
+}
