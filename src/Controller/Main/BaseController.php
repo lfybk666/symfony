a41976f8352d9c85d1becc21ee31d51e4c1dbde0
suchcka90 <danil-sheptalin@mail.ru>
2021-12-02 14:13:27 +0700

    add styles and bootstrap

--- src/Controller/Main/BaseController.php
+++ src/Controller/Main/BaseController.php
@@ -11,7 +11,7 @@ class BaseController extends AbstractController
     public function renderDefault()
     {
         return [
-            'title' => 'Значение по умолчанию'
+            'title' => 'Сайт learning'
         ];
     }
 }
